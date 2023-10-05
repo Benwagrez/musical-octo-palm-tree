dd3093cbbe58e4a96d15f0c383bd21fd5a4f3aaa
benwagrez <benwagrez@gmail.com>
2023-10-04 22:33:19 -0400

    Adding destory sh script

---
+++ destroy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Installing Python dependencies into packages and zipping the deployment
+pip install --target ./packages -r requirements.txt --no-user
+7z a -tzip ./lambda_function_payload.zip ./packages/*
+7z a ./lambda_function_payload.zip ./python/*
+
+# Running terraform apply
+terraform destroy -var-file="terraform.tfvars"
+
+# Cleaning up working directory
+rm lambda_function_payload.zip
